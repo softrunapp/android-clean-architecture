6d6ea0d747967f0945a45546154a012eb4139831
meisam <softrunapps@gmail.com>
2019-09-25 17:21:44 +0330

    add dagger

--- domain/src/main/java/com/test/domain/intractor/GetUserById.java
+++ domain/src/main/java/com/test/domain/intractor/GetUserById.java
@@ -3,6 +3,9 @@ package com.test.domain.intractor;
 import com.test.domain.model.User;
 import com.test.domain.repository.UserRepository;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import io.reactivex.Observable;
 import io.reactivex.Scheduler;
 
@@ -10,7 +13,8 @@ public class GetUserById extends UseCase<User> {
     private final UserRepository userRepository;
     private int userId = 1;
 
-    GetUserById(Scheduler executorThread, Scheduler uiThread, UserRepository userRepository) {
+    @Inject
+    GetUserById(@Named("executor_thread") Scheduler executorThread,@Named("ui_thread") Scheduler uiThread, UserRepository userRepository) {
         super(executorThread, uiThread);
         this.userRepository = userRepository;
     }
