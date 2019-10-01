62281e4c855faa95d0d7c67d6bd531fba13897a1
meisam <softrunapps@gmail.com>
2019-10-01 13:04:33 +0330

    add mockito test

---
+++ domain/src/test/java/com/softrunapp/domain/intractor/GetUserListTest.java
@@ -0,0 +1,45 @@
+package com.softrunapp.domain.intractor;
+
+import com.softrunapp.domain.repository.UserRepository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import io.reactivex.schedulers.Schedulers;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+public class GetUserListTest {
+    private GetUserList getUserList;
+    @Mock
+    UserRepository userRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        getUserList = new GetUserList(
+                Schedulers.trampoline(), Schedulers.trampoline(), userRepository);
+    }
+
+    @Test
+    public void givenAConcreteUseCaseOfGetUserList() {
+        assertThat(getUserList, instanceOf(GetUserList.class));
+    }
+
+    @Test
+    public void getUserListObservableFromUserRepository() {
+        getUserList.createUseCaseObservable();
+        verify(userRepository).userList();
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
